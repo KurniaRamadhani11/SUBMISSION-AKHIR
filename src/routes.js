a47d63bc2ab79d932b1b0a609e42c02782250329
kurnia <kurniaraaaaa79@gmail.com>
2023-10-05 21:10:13 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,31 @@
-const routes = [];
+/* eslint-disable object-curly-newline */
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler, updateBookHandler, delBookHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'post',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'get',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: updateBookHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: delBookHandler,
+  },
+];
 module.exports = routes;
